7d8051e00b2f00a6ff1945927f107e8b69065e1f
jefersonla <oi_je@hotmail.com>
2016-04-26 01:07:33 -0300

    Modificado o código para melhor assimilação com o pseudocódigo

--- main.c
+++ main.c
@@ -16,21 +16,21 @@ typedef struct{
  * aonde p e q são as frequencias e chaves
  * ! TEM QUE VERIFICAR OS INDICES... quando é 0 e quando é 1
  */
-void optimalBST(Chaves *chave, int n, int *e[n + 1], int *root[n]){
+void optimalBST(int *p, int *q, int n, int *e[n + 1], int *root[n]){
     int i, l, j, r, t;
     int w[n+1][n];
 
     // Inicializa os valores - Falta verificar a questão dos indices
     for(i = 0; i < (n + 1); i++){
-        e[i][i - 1] = chave[i-1].chave;
-        w[i][i - 1] = chave[i-1].chave;
+        e[i][i - 1] = q[i-1];
+        w[i][i - 1] = q[i-1];
     }
 
     for(l = 0; l < n; l++){
         for(i = 0; i < (n - l + 1); i++){
             j = i + l - 1;
             e[i][j] = -1;
-            w[i][j] = w[i][j - 1] + chave[j].chave + chave[j].freq;
+            w[i][j] = w[i][j - 1] + q[j] + p[j];
 
             for(r = i; r <= j; r++){
                 t = e[i][r - 1] + e[r + 1][j] + w[i][j];
@@ -41,7 +41,6 @@ void optimalBST(Chaves *chave, int n, int *e[n + 1], int *root[n]){
             }
         }
     }
-
 }
 
 int main(int argc, char *argv[]){
