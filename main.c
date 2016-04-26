4669775cb5ec796f6937008659fbaa4194db4d9a
jefersonla <oi_je@hotmail.com>
2016-04-25 23:55:33 -0300

    Consertado CMakeLists

--- main.c
+++ main.c
@@ -5,14 +5,18 @@
 #include "libs/arvore.h"
 
 int main(int argc, char *argv[]){
-    int x;
+    int n, i;
     char opcao[14];     // Opção
     ArvoreInt a;        // Arvore de Inteiros
 
     // Inicializa a arvore
     inicializa(&a);
 
-    printf("Hello Clion!");
+    // Numero de entradas
+    scanf("%d", &n);
+    for(i = 0; i < n; i++){
+        
+    }
 
     return EXIT_SUCCESS;
 }
